fb6d611e03550b74647bffef4d9a5528380fe2f7
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-01-06 17:50:35 +0100

    rewrite links in documents

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -1,11 +1,92 @@
 /* global */
 /* eslint-disable no-console, class-methods-use-this */
 
-const pageRegex = /^\/content\/adaptto(.+)\/en(.+)?\.helix(-(.+))?\.html$/;
+const targetHostName = 'https://main--adaptto-website--adaptto.hlx.page';
+
+const pageHelixRegex = /^\/content\/adaptto(.+)\/en(.+)?\.helix(-(.+))?\.html$/;
+const pageRegex = /^\/content\/adaptto(.+)\/en(.+)?\.html$/;
 const speakerRegex = /^\/content\/dam\/adaptto\/production\/speaker\/(.+)\.helix(\.(.+))?\.html$/;
 const suffixFragmentRegex = /^fragment-(.+)$/;
 
+/**
+ * Transforms AEM URLs to Helix URLs:
+ * - Removes .helix selector and html extensions
+ * - Detects suffix after helix selector for nav, footer and fragments
+ * - Removes "en" from hierarchy
+ * - Special treatment for speaker pages
+ */
+function transformUrlToPath(url) {
+  let pathname = url;
+  if (url.includes('://')) {
+    pathname = new URL(url).pathname;
+  }
+
+  const pageHelixMatch = pathname.match(pageHelixRegex);
+  if (pageHelixMatch) {
+    const year = pageHelixMatch[1];
+    const localPath = pageHelixMatch[2] ?? '/index';
+    const suffix = pageHelixMatch[4];
+    if (suffix) {
+      const suffixFragmentMatch = suffix.match(suffixFragmentRegex);
+      if (suffixFragmentMatch) {
+        return `${year}/fragments/${suffixFragmentMatch[1]}`;
+      }
+      return `${year}/${suffix}`;
+    }
+    return `${year}${localPath}`;
+  }
+
+  const pageMatch = pathname.match(pageRegex);
+  if (pageMatch) {
+    const year = pageMatch[1];
+    const localPath = pageMatch[2] ?? '/index';
+    return `${year}${localPath}`;
+  }
+
+  const speakerMatch = pathname.match(speakerRegex);
+  if (speakerMatch) {
+    const speaker = speakerMatch[1];
+    const variation = speakerMatch[3];
+    if (variation) {
+      return `/speakers/${speaker}-${variation}`;
+    }
+    return `/speakers/${speaker}`;
+  }
+
+  return pathname;
+}
+
+function rewriteLinks(document) {
+  const links = document.querySelectorAll('a');
+  if (links) {
+    links.forEach((anchor) => {
+      let url = targetHostName + transformUrlToPath(anchor.href);
+      if (url.endsWith('/index')) {
+        url = url.replace(/\/index$/, '/');
+      }
+      anchor.href = url;
+    });
+  }
+}
+
 export default {
+
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
+  transformDOM: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    rewriteLinks(document);
+    return document.body;
+  },
 
   /**
    * Return a path that describes the document being transformed (file name, nesting...).
@@ -19,35 +100,6 @@ export default {
   generateDocumentPath: ({
     // eslint-disable-next-line no-unused-vars
     document, url, html, params,
-  }) => {
-    const { pathname } = new URL(url);
-
-    const pageMatch = pathname.match(pageRegex);
-    if (pageMatch) {
-      const year = pageMatch[1];
-      const localPath = pageMatch[2] ?? '/index';
-      const suffix = pageMatch[4];
-      if (suffix) {
-        const suffixFragmentMatch = suffix.match(suffixFragmentRegex);
-        if (suffixFragmentMatch) {
-          return `${year}/fragments/${suffixFragmentMatch[1]}`;
-        }
-        return `${year}/${suffix}`;
-      }
-      return `${year}${localPath}`;
-    }
-
-    const speakerMatch = pathname.match(speakerRegex);
-    if (speakerMatch) {
-      const speaker = speakerMatch[1];
-      const variation = speakerMatch[3];
-      if (variation) {
-        return `/speakers/${speaker}-${variation}`;
-      }
-      return `/speakers/${speaker}`;
-    }
-
-    return pathname;
-  },
+  }) => transformUrlToPath(url),
 
 };
