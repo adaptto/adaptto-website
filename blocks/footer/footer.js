9de7396e8cca768de3eea982504f69fbfad3a1ff
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-01-16 13:40:14 +0100

    refactor footer css

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -6,38 +6,14 @@ import { getSiteRoot } from '../../scripts/site-utils.js';
  * @param {Element} footerNav
  */
 function decorateFooterNav(footerNav) {
-  footerNav.classList.add('row');
-
-  // get first level of ul/li elements and rebuild them as section with h2 headline
-  // and the nested ul as actual navigation list
-  const columns = footerNav.querySelector(':scope > ul');
-  if (columns) {
-    footerNav.removeChild(columns);
-    Array.from(columns.children).forEach((column) => {
-      const section = append(footerNav, 'section', 'col-1-4');
-      const boxPadding = append(section, 'div', 'box-padding');
-
-      const h2 = append(boxPadding, 'h2', 'title', 'title-section', 'title-footer');
-      h2.textContent = Array.from(column.childNodes)
-        .find((node) => node.nodeType === Node.TEXT_NODE)?.textContent.trim();
-
-      const navList = column.querySelector('ul');
-      if (navList) {
-        navList.classList.add('navlist', 'navlist-footer');
-        navList.querySelectorAll('a').forEach((a) => a.classList.add('navlink', 'navlink-footer'));
-        boxPadding.append(navList);
-      }
-    });
-  }
+  footerNav.classList.add('section-footernav', 'row');
 }
 
 /**
  * @param {Element} footerText
  */
 function decorateFooterText(footerText) {
-  footerText.classList.add('row');
-  footerText.querySelectorAll('p').forEach((p) => p.classList.add('footer-text', 'box-padding'));
-  footerText.querySelectorAll('a').forEach((a) => a.classList.add('navlink'));
+  footerText.classList.add('section-footertext', 'row');
 }
 
 /**
@@ -54,7 +30,7 @@ export default async function decorate(block) {
   const resp = await fetch(`${navPath}.plain.html`);
   if (resp.ok) {
     const html = await resp.text();
-    const container = append(block, 'div', 'footer-site');
+    const container = append(block, 'div');
     container.innerHTML = html;
 
     // first section: footer navigation
