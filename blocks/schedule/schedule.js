fb1f736c2ed3bd71b3ac916f04962025d91d4edd
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-01-18 16:59:54 +0100

    schedule tab navigation

--- blocks/schedule/schedule.js
+++ blocks/schedule/schedule.js
@@ -3,22 +3,55 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
 import { getScheduleData } from '../../scripts/ScheduleData.js';
 import { getSiteRoot } from '../../scripts/site-utils.js';
 
+const dayIdPattern = /^#day-(\d)$/;
+
+/**
+ * Gets the active day from current location hash.
+ * @returns {number} Active day or undefined.
+ */
+function getActiveDayFromHash() {
+  const dayIdMatch = window.location.hash.match(dayIdPattern);
+  if (dayIdMatch) {
+    return parseInt(dayIdMatch[1], 10);
+  }
+  return undefined;
+}
+
+/**
+ * Displays schedule for given day.
+ * @param {Element} block
+ * @param {number} day
+ */
+function displayDay(block, day) {
+  block.querySelectorAll('a.active').forEach((a) => a.classList.remove('active'));
+  block.querySelector(`a[rel="id-day-${day}"]`)?.classList.add('active');
+  block.querySelectorAll('.tab-content.active').forEach((div) => div.classList.remove('active'));
+  block.querySelector(`#id-day-${day}`)?.classList.add('active');
+}
+
 /**
  * Build tab navigation links.
  * @typedef {import('../../scripts/ScheduleDay').default} ScheduleDay
  * @param {Element} element
  * @param {ScheduleDay[]} days
+ * @param {number} activeDay
  */
-function buildTabNavigation(parent, days) {
+function buildTabNavigation(parent, days, activeDay) {
   const tabNav = append(parent, 'div', 'tab-navigation');
   days.forEach((day) => {
     const link = append(tabNav, 'a');
     link.href = `#day-${day.day}`;
-    link.rel = `day-${day.day}`;
+    link.rel = `id-day-${day.day}`;
     link.textContent = `Day ${day.day}`;
-    if (day.day === 1) {
+    if (day.day === activeDay) {
       link.classList.add('active');
     }
+
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      displayDay(parent, day.day);
+      window.history.pushState(null, null, `#day-${day.day}`);
+    });
   });
 }
 
@@ -57,11 +90,12 @@ function buildDayEntryRow(tbody, entry) {
  * @typedef {import('../../scripts/ScheduleDay').default} ScheduleDay
  * @param {Element} parent
  * @param {ScheduleDay} day
+ * @param {number} activeDay
  */
-function buildDaySchedule(parent, day) {
+function buildDaySchedule(parent, day, activeDay) {
   const tabContent = append(parent, 'div', 'tab-content');
-  tabContent.id = `day-${day.day}`;
-  if (day.day === 1) {
+  tabContent.id = `id-day-${day.day}`;
+  if (day.day === activeDay) {
     tabContent.classList.add('active');
   }
 
@@ -97,7 +131,22 @@ export default async function decorate(block) {
   const queryIndexUrl = cfg.queryindexurl || '/query-index.json';
   const scheduleData = await getScheduleData(scheduleDataUrl, queryIndexUrl);
 
+  // detect active day
+  let activeDay = getActiveDayFromHash();
+  if (!activeDay) {
+    activeDay = 1;
+    window.history.replaceState(null, null, `#day-${activeDay}`);
+  }
+
+  // react to stage changes via hash
+  window.addEventListener('hashchange', () => {
+    const day = getActiveDayFromHash();
+    if (day) {
+      displayDay(block, day);
+    }
+  });
+
   const days = scheduleData.getDays();
-  buildTabNavigation(block, days);
-  days.forEach((day) => buildDaySchedule(block, day));
+  buildTabNavigation(block, days, activeDay);
+  days.forEach((day) => buildDaySchedule(block, day, activeDay));
 }
