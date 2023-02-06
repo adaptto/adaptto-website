774e73f62509cbc6778feee6479808788e4c97e6
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-06 17:13:40 +0100

    use custom youtube play button

--- blocks/embed-youtube/embed-youtube.js
+++ blocks/embed-youtube/embed-youtube.js
@@ -1,23 +1,22 @@
 // pattern from https://gist.github.com/deunlee/0b45cfacb7e8f788e5bbfa2911f54d3e
 const youTubeUrlPattern = /^(https?:)?(\/\/)?((www\.|m\.)?youtube(-nocookie)?\.com\/((watch)?\?(feature=\w*&)?vi?=|embed\/|vi?\/|e\/)|youtu.be\/)([\w-]{10,20})/i;
 
-const embedYoutube = (vid, autoplay) => {
+function embedYoutube(vid, autoplay) {
   const suffix = autoplay ? '&muted=1&autoplay=1' : '';
-  return `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-    <iframe src="https://www.youtube.com/embed/${vid}?rel=0&v=${vid}${suffix}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
+  return `<div style="left:0; width:100%; height:0; position:relative; padding-bottom:56.25%;">
+    <iframe src="https://www.youtube.com/embed/${vid}?rel=0${suffix}" style="border:0; top:0; left:0; width:100%; height:100%; position:absolute;" 
     allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture" scrolling="no" title="Content from Youtube" loading="lazy"></iframe>
     </div>`;
-};
+}
 
-const loadEmbed = (block, vid, autoplay) => {
+function loadEmbed(block, vid, autoplay) {
   if (block.classList.contains('embed-is-loaded')) {
     return;
   }
-
   block.innerHTML = embedYoutube(vid, autoplay);
   block.classList = 'block embed-youtube';
   block.classList.add('embed-is-loaded');
-};
+}
 
 /**
  * Get YouTube Video ID from URL.
