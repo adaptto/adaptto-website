d51b49c80f8a1abf2b9773fad4de766a44352c3d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-10 14:38:42 +0100

    Embed Pretix Ticket Shop (#31)

---
+++ test/blocks/embed-pretix/embed-pretix.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it */
+
+import { expect } from '@esm-bundle/chai';
+import decorate from '../../../blocks/embed-pretix/embed-pretix.js';
+import { buildBlock } from '../../../scripts/lib-franklin.js';
+
+describe('blocks/embed-google-maps', () => {
+  it('block', () => {
+    const block = buildBlock('embed-pretix', [
+      ['shop-url', 'https://pretix/shop-url'],
+      ['shop-css-url', 'https://pretix/shop-css-url'],
+      ['script-url', 'https://pretix/script-url'],
+    ]);
+    decorate(block);
+
+    expect(document.head.querySelector('script[src=\'https://pretix/script-url\']')).to.exist;
+    expect(block.querySelector('pretix-widget[event=\'https://pretix/shop-url\']')).to.exist;
+  });
+});
