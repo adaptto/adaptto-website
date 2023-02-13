7de550b7accb1f32271f0ae851b906290d74b6f7
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-13 11:02:48 +0100

    HubSpot Newsletter (#32)

---
+++ blocks/embed-newsletter/embed-newsletter.js
@@ -0,0 +1,26 @@
+/* global hbspt */
+import html from '../../scripts/utils/htmlTemplateTag.js';
+
+/**
+ * Embed Hubspot newsletter signup form.
+ * @param {Element} block
+ */
+export default function decorate(block) {
+  const script = document.createElement('script');
+  script.src = '//js.hsforms.net/forms/v2.js';
+  script.type = 'text/javascript';
+  script.onload = () => {
+    try {
+      hbspt.forms.create({
+        portalId: '3937475',
+        formId: '4d3174fb-0e39-4596-b6a8-4b63caeae340',
+      });
+    } catch (e) {
+      block.insertAdjacentHTML('beforeend', html`<p class="text-warning">
+        Newsletter registration form cannot be displayed - please give consent to 'HubSpot Forms' service.<br>
+        <small>${e}</small>
+      </p>`);
+    }
+  };
+  block.append(script);
+}
