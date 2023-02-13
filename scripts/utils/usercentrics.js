0158b18717ca15a723aed129b8d2ae06e0ffe392
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-13 14:23:22 +0100

    head scripts

--- scripts/utils/usercentrics.js
+++ scripts/utils/usercentrics.js
@@ -1,16 +1,51 @@
+import { append } from "./dom.js";
+
+const enabled = true;
 const settingsId = 'o544Hdz9e';
+
+/**
+ * Checks enablement of consent management.
+ * @returns true if consent management is enabled.
+ */
+export function isConsentManagementEnabled() {
+  return enabled;
+}
 
 /**
  * Enables Usercentrics Consent Management.
  * @param {Element} head HTML Head
  */
-/* eslint-disable-next-line import/prefer-default-export */
-export function enableConsentManagement(head) {
-  head.insertAdjacentHTML('beforeend', `
-    <link rel="preconnect" href="//app.usercentrics.eu">
-    <link rel="preconnect" href="//privacy-proxy.usercentrics.eu">
-    <link rel="preload" href="//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js" as="script">
-    <script id="usercentrics-cmp" data-settings-id="${settingsId}" src="https://app.usercentrics.eu/browser-ui/latest/bundle.js" async></script>
-    <script type="application/javascript" src="https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js"></script>
-  `);
+export function decorateConsentManagement(head) {
+  if (!isConsentManagementEnabled()) {
+    return;
+  }
+
+  const fragment = document.createDocumentFragment();
+
+  // Usercentrics Web CMP v2
+  const preconnect1 = append(fragment, 'link');
+  preconnect1.rel = 'preconnect';
+  preconnect1.href = '//app.usercentrics.eu';
+
+  const preload = append(fragment, 'link');
+  preload.rel = 'preload';
+  preload.as = 'script';
+  preload.href = '//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js';
+
+  const scriptCMP = append(fragment, 'script');
+  scriptCMP.id = 'usercentrics-cmp';
+  scriptCMP.dataset.settingsId = settingsId;
+  scriptCMP.async = true;
+  scriptCMP.src = 'https://app.usercentrics.eu/browser-ui/latest/loader.js';
+
+  // Smart Data Protector
+  const preconnect2 = append(fragment, 'link');
+  preconnect2.rel = 'preconnect';
+  preconnect2.href = '//privacy-proxy.usercentrics.eu';
+
+  const scriptProtector = append(fragment, 'script');
+  scriptProtector.type = 'application/javascript';
+  scriptProtector.src = 'https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js';
+
+  head.append(fragment);
 }
