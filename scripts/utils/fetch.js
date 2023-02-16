09d373d7d1f9419dbee26bda967447c7c0085eb6
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-16 14:11:36 +0100

    fix linting errors

--- scripts/utils/fetch.js
+++ scripts/utils/fetch.js
@@ -1,8 +1,10 @@
 /**
- * Detects the navigation type of the current page in browser. If the current page was reloaded (e.g. by user),
- * force a cache reload of items fetched via Fetch API as well (e.g. footer, header, Query Index).
+ * Detects the navigation type of the current page in browser.
+ * If the current page was reloaded (e.g. by user), force a cache reload of items
+ * fetched via Fetch API as well (e.g. footer, header, Query Index).
  * @returns Options for Fetch API.
  */
+// eslint-disable-next-line import/prefer-default-export
 export function getFetchCacheOptions() {
   const navigationType = window.performance.getEntriesByType('navigation')[0]?.type;
   if (navigationType === 'reload') {
