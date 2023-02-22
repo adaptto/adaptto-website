57c6ff75d12eb833aef9051cfca028e4881859f6
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-22 18:00:09 +0100

    fix indentation

--- blocks/embed-youtube/embed-youtube.js
+++ blocks/embed-youtube/embed-youtube.js
@@ -10,7 +10,7 @@ const youTubeUrlPattern = /^(https?:)?(\/\/)?((www\.|m\.)?youtube(-nocookie)?\.c
  */
 function youtubePlayerHtml(vid) {
   return html`<iframe src="https://www.youtube.com/embed/${vid}?rel=0&muted=1&autoplay=1"
-    allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture"
+     allow="autoplay; fullscreen; picture-in-picture; encrypted-media; accelerometer; gyroscope; picture-in-picture"
      scrolling="no"
      loading="lazy"></iframe>`;
 }
