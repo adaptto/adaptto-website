12e3e34e75a5a82c3c1e59fe087457d362e39e0c
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-02-22 10:08:49 +0100

    show consent also if too much time passed since dom loading

--- scripts/utils/usercentrics.js
+++ scripts/utils/usercentrics.js
@@ -65,6 +65,13 @@ async function decorateConsentDialogMessage(service, parent) {
 }
 
 /**
+ * @returns Time passed since DOM was complete.
+ */
+function getTimeSinceDomComplete() {
+  return Date.now() - window.performance.timing.domComplete;
+}
+
+/**
  * Checks current consent status and re-Decorates block either with
  * actual content, or with consent message.
  * If UserCentrics is not initialized yet, render nothing.
@@ -76,7 +83,9 @@ function decorateDependingOnConsent(service, parent, decorator) {
   parent.innerHTML = '';
   if (isConsentGiven(service)) {
     decorator(parent);
-  } else if (isInitialized) {
+  } else if (isInitialized || getTimeSinceDomComplete() > 1500) {
+    // show consent dialog if consent was not given
+    // or loading of usercentrics scripts to checks this just took too long (>1.5sec)
     decorateConsentDialogMessage(service, parent);
   }
 }
