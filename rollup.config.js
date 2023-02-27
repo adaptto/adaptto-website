c79032d7a191612623ce803a97d296e8f1bdbd89
mbehzad <mehran.behzad@diva-e.com>
2023-02-27 12:24:59 +0100

    DRND-208 add toolig for js bundling to improve load perofrmance

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+const dynamicImportVars = require('@rollup/plugin-dynamic-import-vars');
+const terser = require('@rollup/plugin-terser');
+
+module.exports = {
+  preserveEntrySignatures: true,
+  input: [
+    './scripts/scripts.js',
+    // is already imported by script.js.
+    // There is no need to generate a bundle for it (ignoring 404.html)
+    // './scripts/lib-franklin.js',
+  ],
+  output: {
+    dir: 'target',
+    format: 'esm',
+    plugins: [
+      terser(), // minifies the JS
+    ],
+  },
+  plugins: [
+    dynamicImportVars(), // allows generating assets from import(`path/${var}.js`)
+  ],
+};
