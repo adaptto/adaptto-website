be76da658002a6baca947b2c233266dbb454b12e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-04-27 15:40:07 +0200

    Load header/main navigation at first position in loadLazy (#70)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,7 +17,12 @@ import { append } from './utils/dom.js';
 import { getSiteRootPath, isSpeakerDetailPath } from './utils/site.js';
 import { decorateConsentManagement } from './utils/usercentrics.js';
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
+const LCP_BLOCKS = [
+  'stage-header',
+  'schedule',
+  'image-gallery',
+  'talk-detail-before-outline',
+];
 
 /**
  * Extracts the stage header block and prepends a new section for it.
@@ -217,6 +222,8 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  loadHeader(doc.querySelector('header .header-container'));
+
   const main = doc.querySelector('main');
   await loadBlocks(main);
 
@@ -224,7 +231,6 @@ async function loadLazy(doc) {
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header .header-container'));
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
