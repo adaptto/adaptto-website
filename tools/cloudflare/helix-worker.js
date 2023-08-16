19538355074d70d49d0bc9ce36f8cacfa5ffce54
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-08-16 14:19:37 +0200

    fix schedule short url

--- tools/cloudflare/helix-worker.js
+++ tools/cloudflare/helix-worker.js
@@ -11,7 +11,7 @@ const redirectMap = [
   [new RegExp('^/cfp$'), `/${currentYear}/sign-up/call-for-papers`, 302],
   [new RegExp('^/(tickets|call-for-papers)$'), `/${currentYear}/sign-up/$1`, 302],
   [new RegExp('^/(speaker|program-committee|gallery|code-of-conduct|contact)$'), `/${currentYear}/conference/$1`, 302],
-  [new RegExp('^/(conference|agenda|sponsors|venue|newsletter|archive)$'), `/${currentYear}/$1`, 302],
+  [new RegExp('^/(conference|schedule|sponsors|venue|newsletter|archive)$'), `/${currentYear}/$1`, 302],
   [new RegExp('^/(privacy|email-privacy)$'), `/${currentYear}/privacy/$1`, 302],
   // rewrite old AEM URI schema
   [new RegExp('^/(\\d\\d\\d\\d)/en/?(.*)\\.html$'), '/$1/$2', 301, true],
